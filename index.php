ef7065208e91094d2ade2f24e6530c39fa2a9546
Bense Mate <h846858@stud.u-szeged.hu>
2023-10-23 14:33:01 +0200

    first templates

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
-require_once("./prologue.php");
+require_once("./prologue.php"); // --> foo, const, paths
 
-print "Helloo!";
+// PAGE - LOAD TEMPLATE
+$dom = new DOMDocument();
+$dom->loadHTMLFile(BASE_TEMPLATE); // <-- paths
+
+// PAGE - FILL TEMPLATE // <-- foo
+haveSession("login");
+$page = getPage();
+
+
 ?>
--- libraries/paths.php
+++ libraries/paths.php
@@ -1,9 +1,14 @@
 <?php
+
+// Directories
+
 define("INDEX_DIR", "." . DIRECTORY_SEPARATOR);
 define("ELEM_DIR", INDEX_DIR . "elements" . DIRECTORY_SEPARATOR);
 define("LIB_DIR", INDEX_DIR . "libraries" . DIRECTORY_SEPARATOR);
 define("STYLE_DIR", INDEX_DIR . "styles" . DIRECTORY_SEPARATOR);
 
+//Files
 
+define("BASE_TEMPLATE", ELEM_DIR . "base.htm");
 
 ?>
