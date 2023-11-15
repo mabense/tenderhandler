ed3de6aa81cf265ae23bc8697fd34c7b9e4dd9e6
Bense Mate <h846858@stud.u-szeged.hu>
2023-11-15 03:57:31 +0100

    blob download works

---
+++ download/index.php
@@ -0,0 +1,60 @@
+<?php
+define("ROOT", ".." . DIRECTORY_SEPARATOR);
+define("PAGE", "download");
+
+require_once(ROOT . "const.php");
+require_once(ROOT . "requirements.php");
+
+require_once(LIB_DIR . "sql.php");
+
+haveSession();
+$page = PAGE;
+
+if (!auth(false, true, true)) {
+    redirectTo(ROOT, "home");
+}
+
+$tender = getTender();
+$ms = getMilestone();
+$doc = getDocument();
+
+$success = false;
+
+
+if (
+    isset($tender) &&
+    isset($ms) &&
+    isset($doc)
+) {
+    $GLOBALS["conn"] = sqlConnect();
+
+    $sql = "SELECT `document`, `file_name` FROM DOCUMENT WHERE `tender`=? AND `milestone`=? AND `requirement`=?";
+    $stmt = sqlPrepareBindExecute(
+        $sql,
+        "sis",
+        [$tender, $ms, $doc],
+        __FUNCTION__
+    );
+
+    $result = $stmt->get_result();
+    if ($row = $result->fetch_assoc()) {
+        $fileCont = $row['document'];
+        $fileName = $row['file_name'];
+
+        // header('Content-Description: File Transfer');
+        // header("Content-Type: application/octet-stream;");
+        // header("Content-Disposition: attachment; filename=$fileName");
+        // header('Cache-Control: must-revalidate');
+        // header('Pragma: public');
+        // header('Expires: 0');
+        // readfile($file);
+
+        header("Content-Type: application/force-download");
+        header("Content-Disposition: attachment; filename=$fileName");
+        echo $fileCont;
+        
+        sqlDisconnect();
+    }
+}
+
+redirectTo(ROOT, "document_list");
--- upload/submit.php
+++ upload/submit.php
@@ -15,20 +15,32 @@ if ((count($_FILES) > 0) && (is_uploaded_file($_FILES['docfile']['tmp_name']))) 
 
     $file = file_get_contents($_FILES['docfile']['tmp_name']);
     $fileSize = $_FILES['docfile']['size'];
-    pushFeedbackToLog($fileSize);
-    $isTooBig = $fileSize > 40000000; // 40 * 1024 * 1024 = 41 943 040
+    $fileName = $_FILES['docfile']['name'];
+    // pushFeedbackToLog($fileSize);
+    $isTooBig = $fileSize > 1048576;
+    // 64 * 1024 = 65 536
+    // 1024 * 1024 = 1 048 576
 
     if ($isTooBig) {
-        pushFeedbackToLog($fileSize . " bytes is too big.", true);
+        pushFeedbackToLog(
+            ($fileSize > 1024
+                ? ($fileSize > 1048576
+                    ? number_format($fileSize / 1048576, 2) . " MB"
+                    : number_format($fileSize / 1024, 2) . " KB"
+                )
+                : number_format($fileSize, 2) . " bytes"
+            ) . " is too big.",
+            true
+        );
     } else {
         // $conn = sqlConnect();
         $GLOBALS["conn"] = sqlConnect();
         global $conn;
 
-        $sql = "UPDATE DOCUMENT SET `document`=? WHERE `tender`=? AND `milestone`=? AND `requirement`=?";
-        
+        $sql = "UPDATE DOCUMENT SET `document`=?, `file_name`=? WHERE `tender`=? AND `milestone`=? AND `requirement`=?";
+
         $stmt = $conn->prepare($sql);
-        $stmt->bind_param('bsis', $file, $tender, $ms, $doc);
+        $stmt->bind_param('bssis', $file, $fileName, $tender, $ms, $doc);
         $stmt->send_long_data(0, $file);
         $success = $stmt->execute();
 
